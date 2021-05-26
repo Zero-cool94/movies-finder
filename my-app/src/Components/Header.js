2564dcf3e62baba9be627706778b4d68d830d7ee
zero-cool94 <emadmasoud2017@hotmail.com>
2021-05-26 14:15:29 -0500

    add icon

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > svg": {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
 
 const Header = (props) => {
+  const classes = useStyles();
+
   return (
-    <header className="App-header">
-      <h2>{props.text}</h2>
-    </header>
+    <>
+      <header className="App-header">
+        <h2>{props.text}</h2>
+        <div className={classes.root}>
+          <HomeIcon style={{ fontSize: 40 }} />
+        </div>
+      </header>
+    </>
   );
 };
 
