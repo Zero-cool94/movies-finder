a607a19cd0f1b5df8adc094bce805b32528fe451
zero-cool94 <emadmasoud2017@hotmail.com>
2021-05-26 14:21:01 -0500

    add link to go to home page

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,22 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import SvgIcon from "@material-ui/core/SvgIcon";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    "& > svg": {
-      margin: theme.spacing(2),
-    },
+    "& > svg": {},
   },
 }));
-
-function HomeIcon(props) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
-  );
-}
 
 const Header = (props) => {
   const classes = useStyles();
@@ -24,10 +15,9 @@ const Header = (props) => {
   return (
     <>
       <header className="App-header">
-        <h2>{props.text}</h2>
-        <div className={classes.root}>
-          <HomeIcon style={{ fontSize: 40 }} />
-        </div>
+        <Link color="inherit" href="/">
+          <h2>{props.text}</h2>
+        </Link>{" "}
       </header>
     </>
   );
