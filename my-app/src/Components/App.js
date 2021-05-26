de3af10baa872a036996facd8fbfbdae2afeb373
zero-cool94 <emadmasoud2017@hotmail.com>
2021-05-26 13:59:06 -0500

    fix bug

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -39,7 +39,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <Header text="HOOKED" />
+      <Header text="Movies finder" />
       <Search search={search} />
       <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
